c17567c09c700436f702521acf58d80f3dd70043
SGA Com <ljh1642@gmail.com>
2023-07-21 16:07:01 +0900

    study

--- Algorithm/Algorithm/04_QuickSort/QuickSort.cpp
+++ Algorithm/Algorithm/04_QuickSort/QuickSort.cpp
@@ -20,4 +20,36 @@ void DoQuickSort()
 
 void QuickSort(vector<int>& arr, int left, int right)
 {
+    if (left >= right)
+        return;
+
+    int pivot = left; // 기준값
+    int i = pivot + 1;
+    int j = right;
+    int temp;
+
+    while (i <= j)
+    {
+        while (i <= right && arr[i] <= arr[pivot]) // 키 값보다 큰 값 만날때까지 오른쪽으로 이동
+            i++;
+        while (j > left && arr[j] >= arr[pivot]) // 키 값보다 작은 값 만날 때까지 왼쪽으로 이동
+            j--;
+
+        if (i > j) // 엇갈린 상태라면 pivot 값 교체
+        {
+            temp = arr[j];
+            arr[j] = arr[pivot];
+            arr[pivot] = temp;
+        }
+        else
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // 재귀 호출을 통한 분할 계산
+    QuickSort(arr, left, j - 1);
+    QuickSort(arr, j + 1, right);
 }
